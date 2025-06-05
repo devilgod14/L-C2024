a55f42dd7bd22d5141749372a79b87995f3d1de7
devilgod14 <davepiyush811@gmail.com>
2025-06-05 10:41:18 +0530

    Implement DivisorMatchC0unter

---
+++ assignment10/src/countNumbers.js
@@ -0,0 +1,68 @@
+/**
+ * @class DivisorMatchCounter
+ * @description A utility class to efficiently count integers 'n' where 'n' and 'n+1'
+ * have the same number of positive divisors.
+ */
+class DivisorMatchCounter {
+  /**
+   * @private
+   * @readonly
+   * @type {number}
+   * @description The maximum value for which divisor counts are precomputed.
+   */
+  #maxPrecomputationLimit;
+
+  /**
+   * @private
+   * @readonly
+   * @type {number[]}
+   * @description An array where #divisorCounts[k] stores the number of positive divisors of k.
+   */
+  #divisorCounts;
+
+  /**
+   * @const {number}
+   * @description The maximum constraint for 'X' as specified in the problem,
+   * used to determine the maximum precomputation limit.
+   */
+  static MAX_PROBLEM_CONSTRAINT_X = 100000;
+
+  constructor(precomputationLimit = DivisorMatchCounter.MAX_PROBLEM_CONSTRAINT_X) {
+    this.#maxPrecomputationLimit = precomputationLimit;
+    // Array size is precomputationLimit + 2 to safely access index (precomputationLimit + 1)
+    this.#divisorCounts = new Array(this.#maxPrecomputationLimit + 2).fill(0);
+    this.#precomputeDivisorCounts();
+  }
+
+  #precomputeDivisorCounts() {
+    // Iterate from 1 up to maxPrecomputationLimit + 1 to cover all potential 'n' and 'n+1' values.
+    for (let i = 1; i <= this.#maxPrecomputationLimit + 1; i++) {
+      // For each number 'i', increment the count for all its multiples 'j'.
+      // This correctly calculates the number of divisors for each 'j'.
+      for (let j = i; j <= this.#maxPrecomputationLimit + 1; j += i) {
+        this.#divisorCounts[j]++;
+      }
+    }
+  }
+
+  /**
+   * @public
+   * @description Returns the pre-computed number of positive divisors for a given integer.
+   * @param {number} num - The integer for which to get the divisor count.
+   * @returns {number} The number of positive divisors of 'num'.
+   * @throws {Error} If 'num' is out of the precomputed range or is not a positive integer.
+   */
+  getDivisorCountOf(num) {
+    if (num < 1 || num > this.#maxPrecomputationLimit + 1) {
+      throw new Error(`Number ${num} is outside the precomputed range [1, ${this.#maxPrecomputationLimit + 1}] or invalid.`);
+    }
+    return this.#divisorCounts[num];
+  }
+
+  countMatchingDivisors(X) {
+    // To be implemented
+    return 0;
+  }
+}
+
+module.exports = DivisorMatchCounter;
